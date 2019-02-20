257abee891df2c0159187cd1b916240f3eefb121
albaruvi <arv_9m_00@hotmail.es>
2019-02-20 17:31:24 +0100

    client

---
+++ P2/client_2.py
@@ -0,0 +1,27 @@
+# Programming our first client
+
+import socket
+from P1.Seq import Seq
+
+# Create a socket for communicating with the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('Socket created')
+PORT = 8080
+IP = '212.128.253.66'
+
+while True:
+    sequence = Seq(input('Introduce a sequence:  '))
+
+    complement = sequence.complement()
+    reverse = sequence.reverse()
+    # Connect to the server
+    s.connect((IP, PORT))
+    s.send(str.encode('The complement of the sequence is: {}'.format(complement)))
+    s.send(str.encode('\n'))
+    s.send(str.encode('The reverse of the sequence is: {}'.format(reverse)))
+    msg = s.recv(2048).decode('utf-8')
+    print('MESSAGE FROM THE SERVER:')
+    print(msg)
+
+    s.close()
