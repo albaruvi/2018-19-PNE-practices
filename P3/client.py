1f8f4170aa271f42ea4193d6c62a4aa25f01c56d
albaruvi <arv_9m_00@hotmail.es>
2019-02-27 18:28:10 +0100

    client for practice 3

--- P3/client.py
+++ P3/client.py
@@ -1,8 +1,8 @@
 import socket
 
 # SERVER IP, PORT
-PORT = 8080
-IP = '212.128.253.66'
+PORT = 8081
+IP = '212.128.253.69'
 
 # Create the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,24 +10,22 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # establish the connection to the Server (IP, PORT)
 s.connect((IP, PORT))
 
-while True:
+# Before connecting to the server, ask the user for the string
+msg = """atgc"""
 
-    # Before connecting to the server, ask the user for the string
-    msg = input("> ")
+# Now we can create the socket and connect to the servewr
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Now we can create the socket and connect to the servewr
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
 
-    # establish the connection to the Server (IP, PORT)
-    s.connect((IP, PORT))
+# Send the request message to the server
+s.send(str.encode(msg))
 
-    # Send the request message to the server
-    s.send(str.encode(msg))
+# Receive the servers responses
+response = s.recv(2048).decode()
 
-    # Receive the servers responses
-    response = s.recv(2048).decode()
+# Print the server's response
+print("Response: {}".format(response))
 
-    # Print the server's response
-    print("Response: {}".format(response))
-
-    s.close()
+s.close()
